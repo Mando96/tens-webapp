bc804b5679a8f5e2867e1ca4968f94f4c05d85cc
Oluwaseun Okeowo <oluwaseunokeowo@Oluwaseuns-Air.lan>
2024-04-06 14:21:19 -0500

    adding new code

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,77 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+function flipCard() {
+    inRound = true
+  
+    const btnEle1 = document.querySelector(".btn1")
+    const btnEle2 = document.querySelector(".btn2")
+    const playerCard = playerDeck.pop()
+    const computerCard = computerDeck.pop()
+    const alphaCard = []
+    const alphaDeckCount = alphaDeck.numberOfCards + 2
+  
+   // btnEle1.addEventListener("click", () => { playerCardSlot.appendChild(playerCard.getHTML()) })
+    //btnEle2.addEventListener("click", () => { computerCardSlot.appendChild(computerCard.getHTML())})
     
-}
-start
-while (gameover != 0) {
-    
-}
-gameover = 0
+
+    /* Adding the Card to the Slot */
+    computerCardSlot.appendChild(computerCard.getHTML())
+    playerCardSlot.appendChild(playerCard.getHTML())
+    //alphaCardSlot.appendChild(alphaCard.getHTML())
+  
+
+    /* What to do if the Function isRound is true*/
+
+    if (isRoundWinner(playerCard, computerCard, alphaCard)) {
+      text.innerText = "Win"
+      for (let i = 0; i < alphaDeckCount; i++) {
+        playerDeck.push(alphaCard)
+        alphaDeck.pop(alphaCard)
+      }
+    }
+    else if (isRoundWinner(computerCard, playerCard, alphaCard)) {
+      text.innerText = "Lose"
+      for (let i = 0; i < alphaDeckCount; i++) {
+        computerDeck.push(alphaCard)
+        alphaDeck.pop(alphaCard)
+      }
+    }
+     else {
+      text.innerText = "Stale"
+      alphaDeck.push(computerCard)
+      alphaDeck.push(playerCard)
+    }
+
+    /* What to do if the Function isGameover is true */
+  
+    if (isGameOver(playerDeck)) {
+      text.innerText = "You Lose!!"
+      stop = true
+    } else if (isGameOver(computerDeck)) {
+      text.innerText = "You Win!!"
+      stop = true
+    }
+  }
+
+  
+  if (stop) {
+    startGame()
+    return
+  }
+
+  if (inRound) {
+    cleanBeforeRound()
+  } else {
+    flipCards()
+  }
+
+i = 0
+/*  while (i < numberOfCards ) {
+    allow player to press btn
+    => flip card2
+    if(card1 is fliped){
+      flip card2
+    }
+    else{
+      waiting for player 1
+    }
+    i++;
+  }*/
--- script.js
+++ script.js
@@ -1,142 +1,172 @@
 import Deck from "./deck.js"
 
-const CARD_VALUE_MAP = { 
- "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
- "11": 12, "12": 12, "13": 13, "14": 14, "15": 15, "16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
- "21": 21, "22": 22, "23": 23, "24": 24, "25": 25, "26": 26, "27": 27, "28": 28, "29": 29, "30": 30,
- "31": 31, "32": 32, "33": 33, "34": 34, "35": 35, "36": 36, "37": 37, "38": 38, "39": 39, "40": 40,
- "41": 41, "42": 42, "43": 43, "44": 44, "45": 45, "46": 46, "47": 47, "48": 48, "49": 49, "40": 40,
- "51": 51, "52": 52, "53": 53, "54": 54, "55": 55, "56": 56, "57": 57, "58": 58, "59": 59, "60": 60 
-}
-
-const computerCardSlot = document.querySelector(".computer-card-slot")
-const playerCardSlot = document.querySelector(".player-card-slot")
-const alphaCardSlot = document.querySelector(".alpha-card-slot")
-const computerDeckElement = document.querySelector(".computer-deck")
-const playerDeckElement = document.querySelector(".player-deck")
-const alphaDeckElement = document.querySelector(".alpha-deck")
-const text = document.querySelector(".text")
 const btnEle = document.querySelector(".btn")
-
-let playerDeck, alphaDeck, computerDeck, inRound, stop
 
 function gameon() {
 
-document.addEventListener("click", () => {
-  if (stop) {
+  const CARD_VALUE_MAP = { 
+  "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10,
+  "11": 12, "12": 12, "13": 13, "14": 14, "15": 15, "16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
+  "21": 21, "22": 22, "23": 23, "24": 24, "25": 25, "26": 26, "27": 27, "28": 28, "29": 29, "30": 30,
+  "31": 31, "32": 32, "33": 33, "34": 34, "35": 35, "36": 36, "37": 37, "38": 38, "39": 39, "40": 40,
+  "41": 41, "42": 42, "43": 43, "44": 44, "45": 45, "46": 46, "47": 47, "48": 48, "49": 49, "40": 40,
+  "51": 51, "52": 52, "53": 53, "54": 54, "55": 55, "56": 56, "57": 57, "58": 58, "59": 59, "60": 60 
+  }
+
+  const computerCardSlot = document.querySelector(".computer-card-slot")
+  const playerCardSlot = document.querySelector(".player-card-slot")
+  const alphaCardSlot = document.querySelector(".alpha-card-slot")
+  const computerDeckElement = document.querySelector(".computer-deck")
+  const playerDeckElement = document.querySelector(".player-deck")
+  const alphaDeckElement = document.querySelector(".alpha-deck")
+  const text = document.querySelector(".text")
+  const thetime = 500
+  const btnEle3 = document.querySelector(".btn3")
+
+  let playerDeck, alphaDeck, computerDeck, inRound, stop
+
+
+  btnEle3.addEventListener("click", () => {
+    if (stop) {
+      realone()
+      return
+    }
+
+    if (inRound) {
+      cleanBeforeRound()
+      console.log("Sex")
+    } else {
+      flip()
+    }
+  })
+
+  realone()
+  function realone() {
     startGame()
-    return
+    inRound = false
+    stop = false
   }
 
-  if (inRound) {
+  function startGame() {
+    const deck = new Deck()
+    deck.shuffle()
+
+    const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
+    playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
+    computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
+    alphaDeck = new Deck(deck.cards.slice(-1, 0))
+
     cleanBeforeRound()
-  } else {
-    flipCards()
   }
-})
 
-startGame()
-function startGame() {
-  const deck = new Deck()
-  deck.shuffle()
+  function cleanBeforeRound() {
+    inRound = false
+    playerCardSlot.innerHTML = ""
+    computerCardSlot.innerHTML = ""
+    alphaCardSlot.innerHTML = ""
+    text.innerText = ""
 
-  const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
-  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
-  alphaDeck = new Deck(deck.cards.slice(-1, 0))
+    updateDeckCount()
+  }
 
-  inRound = false
-  stop = false
+  function flip() {
+    inRound = true
 
-  cleanBeforeRound()
-}
+    const computerCard = computerDeck.pop()
+    const playerCard = playerDeck.pop()
+    const alphaCard = []
+    const alphaDeckCount = alphaDeck.numberOfCards + 2
 
-function cleanBeforeRound() {
-  inRound = false
-  computerCardSlot.innerHTML = ""
-  playerCardSlot.innerHTML = ""
-  alphaCardSlot.innerHTML = ""
-  text.innerText = ""
+    console.log()
 
-  updateDeckCount()
-}
+    updateDeckCount()
 
-function flipCards() {
-  inRound = true
+    playerCardSlot.appendChild(playerCard.getHTML())
 
-  const btnEle1 = document.querySelector(".btn1")
-  const btnEle2 = document.querySelector(".btn2")
-  const playerCard = playerDeck.pop()
-  const computerCard = computerDeck.pop()
-  const alphaCard = []
-  const alphaDeckCount = alphaDeck.numberOfCards + 2
+    setTimeout(function () {
+      computerCardSlot.appendChild(computerCard.getHTML())
+      const win = document.querySelector(".btn5")
 
- // btnEle1.addEventListener("click", () => { playerCardSlot.appendChild(playerCard.getHTML()) })
-  //btnEle2.addEventListener("click", () => { computerCardSlot.appendChild(computerCard.getHTML())})
+      if (isRoundWinner(playerCard, computerCard, alphaCard)) {
+        text.innerText = "You Rock!!😗"
+        alphaDeck.push(computerCard)
+        alphaDeck.push(playerCard)
+      } else {
+        text.innerText = "Stale"
+        alphaDeck.push(computerCard)
+        alphaDeck.push(playerCard)
+      }
 
-  computerCardSlot.appendChild(computerCard.getHTML())
-  playerCardSlot.appendChild(playerCard.getHTML())
-  //alphaCardSlot.appendChild(alphaCard.getHTML())
+     /* win.addEventListener("click", () => {
 
-  if (isRoundWinner(playerCard, computerCard, alphaCard)) {
-    text.innerText = "Win"
-    for (let i = 0; i < alphaDeckCount; i++) {
-      playerDeck.push(alphaCard)
-      alphaDeck.pop(alphaCard)
+      if (isRoundWinner( playerCard, computerCard, alphaCard)) {
+        text.innerText = "Win"
+        for (let i = 0; i < alphaDeckCount; i++) {
+          playerDeck.push(alphaCard)
+          alphaDeck.pop(alphaCard)
+        }
+      }
+      else if (isRoundWinner(computerCard, playerCard, alphaCard)) {
+        text.innerText = "Lose"
+        for (let i = 0; i < alphaDeckCount; i++) {
+          computerDeck.push(alphaCard)
+          alphaDeck.pop(alphaCard)
+        }
+      }
+      else {
+        text.innerText = "Stale"
+        alphaDeck.push(computerCard)
+        alphaDeck.push(playerCard)
+      }
+
+      })*/
+
+    }, thetime)
+    
+    if (isGameOver(playerDeck)) {
+      text.innerText = "You Lose!!"
+      stop = true
+    } else if (isGameOver(computerDeck)) {
+      text.innerText = "You Win!!"
+      stop = true
     }
+    
   }
-  else if (isRoundWinner(computerCard, playerCard, alphaCard)) {
-    text.innerText = "Lose"
-    for (let i = 0; i < alphaDeckCount; i++) {
-      computerDeck.push(alphaCard)
-      alphaDeck.pop(alphaCard)
+
+  function updateDeckCount() {
+    playerDeckElement.innerText = playerDeck.numberOfCards
+    alphaDeckElement.innerText = alphaDeck.numberOfCards
+    setTimeout(function () {
+      computerDeckElement.innerText = computerDeck.numberOfCards
+    }, thetime)
+  }
+
+
+  function isRoundWinner(cardOne, cardTwo) {
+    let x = CARD_VALUE_MAP[cardOne.suit]
+    let y = CARD_VALUE_MAP[cardTwo.suit]
+    let sum, sub, val
+
+    sum = x + y
+
+    if (x > y) {
+      sub = x - y
+    } else {
+      sub = y - x
     }
-  }
-   else {
-    text.innerText = "Stale"
-    alphaDeck.push(computerCard)
-    alphaDeck.push(playerCard)
-  }
 
-  if (isGameOver(playerDeck)) {
-    text.innerText = "You Lose!!"
-    stop = true
-  } else if (isGameOver(computerDeck)) {
-    text.innerText = "You Win!!"
-    stop = true
-  }
-}
-
-function updateDeckCount() {
-  computerDeckElement.innerText = computerDeck.numberOfCards
-  playerDeckElement.innerText = playerDeck.numberOfCards
-  alphaDeckElement.innerText = alphaDeck.numberOfCards
-}
-
-function isRoundWinner(cardOne, cardTwo) {
-  let x = CARD_VALUE_MAP[cardOne.suit]
-  let y = CARD_VALUE_MAP[cardTwo.suit]
-  let sum, sub, val
-
-  sum = x + y
-
-  if (x > y) {
-    sub = x - y
-  } else {
-    sub = y -x
+    if (sum % 10 === 0 || sub % 10 === 0) {
+      val = true
+    } else {
+      val = false
+    }
+    return val
   }
 
-  if (sum % 10 === 0 || sub % 10 === 0) {
-    val = true
-  } else {
-    val = false
+  function isGameOver(deck) {
+    return deck.numberOfCards === 0
   }
-  return val
-}
 
-function isGameOver(deck) {
-  return deck.numberOfCards === 0
-}
 }
 
 btnEle.addEventListener("click", () => {
